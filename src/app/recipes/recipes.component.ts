64f38b5b76a431ccd6b1f6675ea43714274fa10b
kmaule15 <kaylamaule15@gmail.com>
2023-08-27 21:23:19 -0500

    set up header

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-recipe',
+  selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
 })
